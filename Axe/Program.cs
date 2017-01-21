5475698bd56cb13a4ec6ea2c58080321b4312da6
SteisAndreeva <steis_ka@abv.bg>
2017-01-22 00:05:06 +0200

    Add files via upload

---
+++ Axe/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Axe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var N = int.Parse(Console.ReadLine());
+
+            for (int rown = 0; rown < N; rown++)
+            {
+                Console.WriteLine("{0}*{1}*{2}",new string('-',3*N),new string('-',rown),new string('-', 2*N-2-rown));
+            }
+            for (int rows2 = 0; rows2 < N/2; rows2++)
+            {
+                Console.WriteLine("{0}*{1}*{2}", new string('*', 3 * N), new string('-', N-1), new string ('-', N-1));
+            }
+            for (int row3 = 0; row3 < N/2-1; row3++)
+            {
+                Console.WriteLine("{0}*{1}*{2}", new string('-', 3*N-row3),new string('-', (N-1)+2*row3), new string('-',(N-1)-row3));
+            }
+            Console.WriteLine("{0}{1}{2}", new string('-', 3*N-(N/2-1)), new string('*', 2*N-1-(N%2)), new string ('-',5*N-(3 * N - (N / 2 - 1))-(2 * N - 1 - (N % 2))));
+        }
+    }
+}
---
+++ Hospital/Hospital.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital
+{
+    class Hospital
+    {
+        static void Main(string[] args)
+        {
+            var days = int.Parse(Console.ReadLine());
+
+            int pregledani = 0;
+            int nepregledani = 0;
+            int doctor = 7;
+
+
+
+            for (int day = 1; day <= days; day++)
+            {
+                if (day%3==0 && pregledani<nepregledani)
+                {
+                    doctor++;
+                }
+                        var pacient = int.Parse(Console.ReadLine());
+
+                        if (pacient <= doctor)
+                        {
+                            pregledani += pacient;
+                        }
+                        else
+                        {
+                            pregledani +=  doctor;
+                            nepregledani += (pacient - doctor);
+                        }
+                    }
+            Console.WriteLine("Treated patients: {0}." ,pregledani);
+            Console.WriteLine("Untreated patients: {0}." , nepregledani);
+        }
+    }
+}
